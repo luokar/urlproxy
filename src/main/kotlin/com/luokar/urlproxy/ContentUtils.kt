64ac03322353d089a2c3a51f383efa8e196e7dd6
luokar <luokar@gmail.com>
2021-06-20 22:24:15 +0800

    add deflate compression

--- src/main/kotlin/com/luokar/urlproxy/ContentUtils.kt
+++ src/main/kotlin/com/luokar/urlproxy/ContentUtils.kt
@@ -3,15 +3,17 @@ package com.luokar.urlproxy
 import org.apache.commons.io.IOUtils
 import org.apache.commons.io.input.BOMInputStream
 import org.brotli.dec.BrotliInputStream
+import java.io.BufferedInputStream
 import java.io.InputStream
 import java.nio.charset.Charset
 import java.util.zip.GZIPInputStream
+import java.util.zip.InflaterInputStream
 
 object ContentUtils {
 
     fun decode(inputStream: InputStream, encoding: String? = null, charset: Charset? = null): String {
         val decompressedInputStream = uncompress(inputStream, encoding)
-        val bomInputStream = BOMInputStream(decompressedInputStream)
+        val bomInputStream = BOMInputStream(BufferedInputStream(decompressedInputStream))
         return IOUtils.toString(bomInputStream, charset ?: Charsets.UTF_8)
     }
 
@@ -21,6 +23,8 @@ object ContentUtils {
                 BrotliInputStream(inputStream)
             encoding.equals("gzip", ignoreCase = true) ->
                 GZIPInputStream(inputStream)
+            encoding.equals("deflate", ignoreCase = true) ->
+                InflaterInputStream(inputStream)
             else -> return inputStream
         }
     }
--- src/main/kotlin/com/luokar/urlproxy/UrlProxyApplication.kt
+++ src/main/kotlin/com/luokar/urlproxy/UrlProxyApplication.kt
@@ -10,7 +10,6 @@ import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
 import javax.net.ssl.SSLContext
 import javax.net.ssl.SSLSocketFactory
-import javax.net.ssl.TrustManager
 import javax.net.ssl.X509TrustManager
 
 
@@ -21,7 +20,7 @@ class UrlProxyApplication {
 
     @Bean
     fun client(): OkHttpClient {
-        val trustAllCerts: Array<TrustManager> = arrayOf(
+        val trustAllCerts = arrayOf(
             object : X509TrustManager {
                 override fun checkClientTrusted(chain: Array<X509Certificate?>?, authType: String?) = Unit
                 override fun checkServerTrusted(chain: Array<X509Certificate?>?, authType: String?) = Unit
@@ -33,12 +32,12 @@ class UrlProxyApplication {
         val sslContext = SSLContext.getInstance("SSL")
         sslContext.init(null, trustAllCerts, SecureRandom())
         // Create an ssl socket factory with our all-trusting manager
-        val sslSocketFactory: SSLSocketFactory = sslContext.getSocketFactory()
+        val sslSocketFactory: SSLSocketFactory = sslContext.socketFactory
         return OkHttpClient
             .Builder()
             .writeTimeout(1800, TimeUnit.SECONDS)
             .readTimeout(1800, TimeUnit.SECONDS)
-            .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+            .sslSocketFactory(sslSocketFactory, trustAllCerts.first())
             .hostnameVerifier { _, _ -> true }
             .build()
     }
